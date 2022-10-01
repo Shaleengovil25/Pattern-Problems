7c39e8ecde1ba16dcfeaf04794f23c394c48cfee
Nandini Govil <72183982+Nandinigovil05@users.noreply.github.com>
2022-10-01 10:30:20 +0530

    Create Inv Pyramid.cpp

---
+++ Inv Pyramid.cpp
@@ -0,0 +1,32 @@
+// Problem:
+// for a given input n, print an inverted pyramid.
+
+// Sample:
+// for n = 6, output should be
+
+// ***********
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
+
+// Solution:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n; cin >> n;
+	for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+	        cout << " ";
+	    }
+	    for(int j=2*n-1;j>2*i;j--){
+	        cout << "*";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}
